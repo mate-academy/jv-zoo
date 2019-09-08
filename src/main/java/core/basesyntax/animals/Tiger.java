e9578ece1b106616d06ddc8d65ddd1cb8942e42f
joej0e <36197513+joej0e@users.noreply.github.com>
2019-09-08 15:38:18 +0300

    Update Tiger.java

--- src/main/java/core/basesyntax/animals/Tiger.java
+++ src/main/java/core/basesyntax/animals/Tiger.java
@@ -1,6 +1,6 @@
 package core.basesyntax.animals;
 
-public class Tiger extends Animal{
+public class Tiger extends Animal {
     @Override
     public void feed() {
         System.out.println("I'm tiger and I like to eat meat!");
