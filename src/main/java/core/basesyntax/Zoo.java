f44491676cd5ab421901ad7958560a159db27573
joej0e <36197513+joej0e@users.noreply.github.com>
2019-09-08 15:41:20 +0300

    Update Zoo.java

--- src/main/java/core/basesyntax/Zoo.java
+++ src/main/java/core/basesyntax/Zoo.java
@@ -9,34 +9,39 @@ import core.basesyntax.interfaces.Swimable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Zoo<T extends Animal>{
+public class Zoo<T extends Animal> {
     private List<T> animalsHome = new ArrayList<>();
 
     public void addAnimal(AnimalType animalType) {
         animalsHome.add(AnimalFactory.animalFactory(animalType));
     }
+
     public void feedAll() {
-        for(T animal : animalsHome) {
+        for (T animal : animalsHome) {
             animal.feed();
         }
     }
 
     private List<T> birdsHome = new ArrayList<>();
+
     public void addBird(Flyable f) {
         birdsHome.add((T) f);
     }
+
     public void feedBirds() {
-        for(T animal : birdsHome) {
+        for (T animal : birdsHome) {
             animal.feed();
         }
     }
 
     private List<T> fishHome = new ArrayList<>();
+
     public void addFish(Swimable s) {
         birdsHome.add((T) s);
     }
+
     public void feedFish() {
-        for(T animal : fishHome) {
+        for (T animal : fishHome) {
             animal.feed();
         }
     }
