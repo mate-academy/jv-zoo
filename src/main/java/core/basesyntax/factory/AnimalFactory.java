47133548cd5e3f0fb3ad4b7868a2d40ebb89c663
joej0e <36197513+joej0e@users.noreply.github.com>
2019-09-09 16:33:41 +0300

    Update AnimalFactory.java

--- src/main/java/core/basesyntax/factory/AnimalFactory.java
+++ src/main/java/core/basesyntax/factory/AnimalFactory.java
@@ -1,9 +1,9 @@
 package core.basesyntax.factory;
 
 import core.basesyntax.animals.Shark;
-import core.basesyntax.animals.Tiger;
 import core.basesyntax.animals.Penguin;
 import core.basesyntax.animals.Sparrow;
+import core.basesyntax.animals.Tiger;
 import core.basesyntax.enumerate.AnimalType;
 import core.basesyntax.interfaces.Feedable;
 
