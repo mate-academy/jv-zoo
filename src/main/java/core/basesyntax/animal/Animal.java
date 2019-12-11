7462a3acd22954b434ec575896419849696d6543
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-11 14:23:04 +0200

    resolved checkstyle

--- src/main/java/core/basesyntax/animal/Animal.java
+++ src/main/java/core/basesyntax/animal/Animal.java
@@ -1,12 +1,12 @@
 package core.basesyntax.animal;
-
 /**
  * Feel free to remove this class and create your own.
  */
+
 public abstract class Animal {
     public String name;
 
-    public Animal(String name){
+    public Animal(String name) {
         this.name = name;
     }
 
