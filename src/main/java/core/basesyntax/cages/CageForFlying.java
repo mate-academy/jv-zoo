03e276ecd96e0d3c84730ff707be92469f13e9f6
Roman Dubovskyi <rdubowskij@gmail.com>
2019-12-11 16:25:53 +0200

    overrided feed method, made restricted generics for cages

--- src/main/java/core/basesyntax/cages/CageForFlying.java
+++ src/main/java/core/basesyntax/cages/CageForFlying.java
@@ -1,9 +1,8 @@
 package core.basesyntax.cages;
 
-
 import core.basesyntax.behavior.Flyable;
 
-public class CageForFlying<T extends Flyable > {
+public class CageForFlying<T extends Flyable> {
     private T flyingAnimal;
 
     public void addAnimal(T animal) {
