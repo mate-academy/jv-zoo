2f84d8fb4a5c932c4f2a3b7588a9757f881d045e
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-20 12:32:04 +0300

    added interface Flyable to Penguin

--- src/main/java/core/basesyntax/zoo/task/animals/Penguin.java
+++ src/main/java/core/basesyntax/zoo/task/animals/Penguin.java
@@ -1,9 +1,15 @@
 package core.basesyntax.zoo.task.animals;
 
 import core.basesyntax.zoo.task.animals.interfaces.AbleToMoveOnTheLand;
+import core.basesyntax.zoo.task.animals.interfaces.Flyable;
 import core.basesyntax.zoo.task.animals.interfaces.Swimable;
 
-public class Penguin extends Animal implements Swimable, AbleToMoveOnTheLand {
+public class Penguin extends Animal implements Swimable, AbleToMoveOnTheLand, Flyable {
+    @Override
+    public void fly() {
+        throw new UnsupportedOperationException("Penguin cannot fly");
+    }
+
     public Penguin() {
         super("Penguin");
     }
