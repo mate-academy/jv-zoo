e0969a182b6bb0e0a0fe26436391b0d4a0aa8eb2
Yurii Popovych <popovychyb@gmail.com>
2020-04-22 11:43:25 +0300

    made Penguin Swimmable

--- src/main/java/core/basesyntax/animals/Penguin.java
+++ src/main/java/core/basesyntax/animals/Penguin.java
@@ -1,10 +1,16 @@
 package core.basesyntax.animals;
 
 import core.basesyntax.types.Bird;
+import core.basesyntax.types.Swimmable;
 
-public class Penguin extends Bird {
+public class Penguin extends Bird implements Swimmable {
     @Override
     public void eat() {
         System.out.println("Penguin is eating");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Penguin is swimming");
     }
 }
