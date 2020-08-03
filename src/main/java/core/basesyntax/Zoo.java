dfaf39dff84faac8f296c5cb2fa142830e1bb5f0
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-08-03 12:39:56 +0300

    added fix to my Zoo

--- src/main/java/core/basesyntax/Zoo.java
+++ src/main/java/core/basesyntax/Zoo.java
@@ -1,43 +1,24 @@
 package core.basesyntax;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Zoo {
-    private List<Bird> cellBirds = new ArrayList<>();
-    private List<Fish> cellFish = new ArrayList<>();
-    private List<Animal> cellAnimals = new ArrayList<>();
+    public void createAnimal() {
+        ArrayList<Animal> zoo = new ArrayList<>();
+        zoo.add(new Tiger());
+        zoo.add(new Penguin());
+        zoo.add(new Sparrow());
+        zoo.add(new Shark());
 
-    public void addAnimal(Animal animal) {
-        cellAnimals.add(animal);
-    }
-
-    public void addBird(Bird bird) {
-        cellBirds.add(bird);
-    }
-
-    public void addFish(Fish fish) {
-        cellFish.add(fish);
-    }
-
-    public void feedAnimals() {
-        List<Animal> animals = new ArrayList(cellAnimals);
-        for (Animal animal : animals) {
+        for (Animal animal : zoo) {
             animal.feed();
+            if (animal instanceof Floating) {
+                ((Floating) animal).swim();
+            }
+            if (animal instanceof Flying) {
+                ((Flying) animal).fly();
+            }
         }
-    }
 
-    public void feedBirds() {
-        List<Bird> birdsWhoFlying = new ArrayList(cellBirds);
-        for (Bird birdWhoFlying : birdsWhoFlying) {
-            birdWhoFlying.feed();
-        }
-    }
-
-    public void feedFish() {
-        List<Fish> fish = new ArrayList(cellFish);
-        for (Fish fishes : fish) {
-            fishes.feed();
-        }
     }
 }
